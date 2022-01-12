e47edad92abaf8f78e82dd6b041f1fd6b842c356
cherina97 <cherina97@gmail.com>
2022-01-12 16:06:37 +0200

    added task 3

--- src/main/java/com/epam/springbootmodule/task2/SpringBootModuleApplication.java
+++ src/main/java/com/epam/springbootmodule/task2/SpringBootModuleApplication.java
@@ -3,7 +3,7 @@ package com.epam.springbootmodule.task2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringBootModuleApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/epam/springbootmodule/task3/SpringSecurityApp.java
+++ src/main/java/com/epam/springbootmodule/task3/SpringSecurityApp.java
@@ -1,12 +1,12 @@
-package com.epam.springbootmodule.task1;
+package com.epam.springbootmodule.task3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-public class App {
+@SpringBootApplication
+public class SpringSecurityApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(SpringSecurityApp.class, args);
     }
 }
