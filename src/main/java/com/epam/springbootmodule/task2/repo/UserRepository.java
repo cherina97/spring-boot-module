543dcc505822347f6a00f080f2777cc869dd71f5
cherina97 <cherina97@gmail.com>
2022-01-28 13:01:18 +0200

    fixed repositories

--- src/main/java/com/epam/springbootmodule/task2/repo/UserRepository.java
+++ src/main/java/com/epam/springbootmodule/task2/repo/UserRepository.java
@@ -2,15 +2,14 @@ package com.epam.springbootmodule.task2.repo;
 
 import com.epam.springbootmodule.task2.model.User;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Profile("task2")
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
--- src/main/java/com/epam/springbootmodule/task7/repo/UserRepository.java
+++ src/main/java/com/epam/springbootmodule/task7/repo/UserRepository.java
@@ -2,7 +2,7 @@ package com.epam.springbootmodule.task7.repo;
 
 import com.epam.springbootmodule.task7.model.User;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Profile("test")
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
